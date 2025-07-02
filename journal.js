4db0fef8e09804749d2959e6322e97933a3c96b9
Justin Logan <juslog24@gmail.com>
2025-07-02 01:08:46 -0700

    Preliminary completion of 5 functions

--- journal.js
+++ journal.js
@@ -2,3 +2,19 @@
 function greet(name) {
     return "Hello, " + name + "!";
   }
+
+  function bark() {
+    return "Woof!";
+  }
+
+  function plusOne(number) {
+    return number + 1;
+  }
+
+  function stringLength(string) {
+    return string.length;
+  }
+
+  function copyCat(string) {
+    return string;
+  }
